aaf8b34aaebfefafdacba0a05c5eddd59e59ff9c
Daniel Mateus Satiro <mateus_satiro@yahoo.com.br>
2021-09-30 22:12:57 -0300

    implanta barreira para o jogador

--- assets/index.js
+++ assets/index.js
@@ -46,8 +46,8 @@ map.forEach((linha,i) => {
     })
 })
 
-const line = 9
-const col = -1
+let line = 9
+let col = -1
 const sizeCelula = 20
 let boxTop = line*sizeCelula
 let boxLeft = (col+1)*sizeCelula
@@ -55,19 +55,31 @@ let boxLeft = (col+1)*sizeCelula
 document.addEventListener('keydown', (event) => {
     const keyName = event.key;
     if(keyName==='ArrowDown'){
-      boxTop+= sizeCelula
-      jogador.style.top = `${boxTop}px`
+        if(positionCaminho.find(item=>item[0]===line+1&&item[1]===col)){
+            boxTop+= sizeCelula
+            line++ 
+            jogador.style.top = `${boxTop}px`
+        }
     }
     if(keyName==='ArrowUp'){
-      boxTop-= sizeCelula
-      jogador.style.top = `${boxTop}px`
+        if(positionCaminho.find(item=>item[0]===line-1&&item[1]===col)){
+            boxTop-= sizeCelula
+            line--
+            jogador.style.top = `${boxTop}px`
+        }
     }
     if(keyName==='ArrowRight'){
-      boxLeft+= sizeCelula
-      jogador.style.left = `${boxLeft}px`
+        if(positionCaminho.find(item=>item[0]===line&&item[1]===col+1)){
+            boxLeft+= sizeCelula
+            col++
+            jogador.style.left = `${boxLeft}px`
+        }
     }
     if(keyName==='ArrowLeft'){
-      boxLeft-= sizeCelula
-      jogador.style.left = `${boxLeft}px`
+        if(positionCaminho.find(item=>item[0]===line&&item[1]===col-1)){
+            boxLeft-= sizeCelula
+            col--
+            jogador.style.left = `${boxLeft}px`
+        }
     }    
   });
