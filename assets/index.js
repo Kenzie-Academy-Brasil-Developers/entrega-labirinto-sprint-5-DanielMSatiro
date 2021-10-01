e90f14a687f1ed5db11b595d61eee5ba1541ee45
Daniel Mateus Satiro <mateus_satiro@yahoo.com.br>
2021-09-30 21:53:42 -0300

    adiciona listener

--- assets/index.js
+++ assets/index.js
@@ -16,9 +16,6 @@ const map = [
     "WWWWWWWWWWWWWWWWWWWWW",
 ];
 
-//template para desenvolver o labirinto
-
-//Elementos que representam as celulas que são paredes
 const labirinto = document.createElement('div')
 labirinto.classList = 'labirinto'
 document.body.appendChild(labirinto)
@@ -29,13 +26,14 @@ labirinto.appendChild(jogador)
 
 const positionCaminho = []
 
+//template para desenvolver o labirinto
+
 map.forEach((linha,i) => {
     const paredeLinha = document.createElement('div')
     paredeLinha.classList = 'paredeLinha'
     labirinto.appendChild(paredeLinha)
     linha.split('').forEach((celula,j) => {
         if(celula==='W'){
-            console.log(celula)
             const parede = document.createElement('div')
             parede.classList = 'parede'
             paredeLinha.appendChild(parede)
@@ -48,6 +46,28 @@ map.forEach((linha,i) => {
     })
 })
 
+const line = 9
+const col = -1
+const sizeCelula = 20
+let boxTop = line*sizeCelula
+let boxLeft = (col+1)*sizeCelula
 
-//FUNÇÃO INTERCEPTAR TECLAS DAS SETAS
-//USAR UMA FORMA DE ARMAZENAR CA POSIÇÃO ATUAL DA DIV VERMELHA
+document.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+    if(keyName==='ArrowDown'){
+      boxTop+= sizeCelula
+      jogador.style.top = `${boxTop}px`
+    }
+    if(keyName==='ArrowUp'){
+      boxTop-= sizeCelula
+      jogador.style.top = `${boxTop}px`
+    }
+    if(keyName==='ArrowRight'){
+      boxLeft+= sizeCelula
+      jogador.style.left = `${boxLeft}px`
+    }
+    if(keyName==='ArrowLeft'){
+      boxLeft-= sizeCelula
+      jogador.style.left = `${boxLeft}px`
+    }    
+  });
